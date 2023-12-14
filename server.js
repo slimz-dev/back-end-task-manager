c80fbf1289c8b63d1227b763ddb7bbf56f003f14
slimz <hungpj11o2@gmail.com>
2023-12-14 12:23:18 +0700

    extend img limit by 50mb

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ mongoose.connect();
 app.use(morgan('combined'));
 
 //Parser
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
+app.use(bodyParser.json({ limit: '50mb' }));
 
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
