16acf320748897166c2b05e703a1358e140bb2c9
slimz <hungpj11o2@gmail.com>
2024-01-14 16:01:44 +0700

    fix

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 async function connect() {
 	try {
-		await mongoose.connect('mongodb+srv://slimz:123@taskmanagement.2h8fmwv.mongodb.net/', {
+		await mongoose.connect(process.env.DB, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});
